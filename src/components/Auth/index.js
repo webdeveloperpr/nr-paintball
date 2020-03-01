8712d76b561f447be372a316e71b90cbb79e1950
Luis Betancourt <webdeveloperpr@gmail.com>
2020-02-29 23:29:41 -0500

    Finally add facebook authentication

--- src/components/Auth/index.js
+++ src/components/Auth/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Authenticator, withFederated } from 'aws-amplify-react';
+import { Authenticator } from 'aws-amplify-react';
 import Amplify from 'aws-amplify';
 import awsconfig from '../../aws-exports';
 
 Amplify.configure(awsconfig);
 
-
 const LoginForm = (props) => {
   return <Authenticator
     // onStateChange={(authState) => console.log('Authentication changed!', authState)}
-    federated={{ facebook_app_id: '1362689763902038' }}
+    federated={{ facebook_app_id: 1362689763902038 }}
     amplifyConfig={awsconfig}
+    errorMessage={(err) => console.log(err)}
   >
   </Authenticator>
 }
